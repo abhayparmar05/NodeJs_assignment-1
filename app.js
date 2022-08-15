080209654d6a817dffe3e1200b283cf2787e7fc9
abhayparmar05 <abhayparmar587@gmail.com>
2022-08-15 16:40:28 -0700

    fixed issued

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 // api object to access data from api
 const urlLink = {
-    url: 'https://api.github.com/search/repositories?q=is:public&limi=1',
+    url: 'https://api.github.com/search/repositories?q=is:public+forks:%3E=200+language:python',
     // User-Agent request header
     headers: {
         "User-Agent": "request"
@@ -21,10 +21,10 @@ const dataretrival = (urlLink, callback) => {
 
             // storing data in data array if language and forks_count
             response.body.items.forEach(item => {
-                if (item.language === 'Python' && item.forks >= 200) {
+                if (urlLink.url != 0) {
                     dataforks = {
                         name: item.name,
-                        lang: item.language,
+                        language: item.language,
                         description: item.description,
                         html_url: item.html_url,
                         watchers_count: item.watchers_count,
@@ -37,11 +37,19 @@ const dataretrival = (urlLink, callback) => {
                     callback(data)
 
                     if (item.stargazers_count < 2000) {
-                        stargazers = { name: item.name, lang: item.language, description: item.description, html_url: item.html_url, watchers_count: item.watchers_count, stargazers_count: item.stargazers_count, forks_count: item.forks }
+                        stargazers = { name: item.name, 
+                            language: item.language,
+                            description: item.description,
+                            html_url: item.html_url, 
+                            watchers_count: item.watchers_count,
+                            stargazers_count: item.stargazers_count, 
+                            forks_count: item.forks 
+                        }
+                        console.log(description)
                         stargazersData.push(stargazers)
                     }
                 }
-            })
+        })
             callback(stargazersData)
         }
     })
@@ -52,7 +60,8 @@ const csvWriting = createCsvWriter({
     path: './Data.csv',
     header: [
         { id: 'name', title: 'Name' },
-        { id: 'lang', title: 'Language' },
+        { id: 'language', title: 'Language' },
+        { id: 'description', title: 'Description'},
         { id: 'html_url', title: 'Html url' },
         { id: 'watchers_count', title: 'Watchers count' },
         { id: 'stargazers_count', title: 'Stargazers count' },
@@ -63,9 +72,10 @@ const csvWriting = createCsvWriter({
 
 dataretrival(urlLink, (msg) => {
     if (msg.length > 0) {
+        var i = 1
         // if data is recieved then writing into csv
         csvWriting.writeRecords(msg).then(() => {
-            console.log('Data Writen Successful')
+            console.log('data write operation perform '+(i+1))
         }).catch((error)=>{
             console.log(error)
         });
